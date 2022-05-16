200a1150890078097bb456c8034c2bbe250c33ba
CaboFernando <fer.1972@hotmail.com>
2022-05-16 11:03:55 -0300

    - Add entidade Produto, com as dataAnnotations para a criação da tabela usando o migrations

---
+++ Entidades/Entidades/Produto.cs
@@ -0,0 +1,39 @@
+﻿using Entidades.Notificacoes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Entidades
+{
+    [Table("TB_PRODUTO")]
+    public class Produto : Notifica
+    {
+        [Column("ID_COD_PRODUTO")]
+        public int CodigoProduto { get; set; }
+
+        [Column("DESCRICAO_PRODUTO")]
+        public string DescricaoProduto { get; set; }
+
+        [Column("SITUACAO_PRODUTO")]
+        public bool SituacaoProduto { get; set; }
+
+        [Column("DATA_FABRICACAO")]
+        public DateTime DataFrabricacao { get; set; }
+
+        [Column("DATA_VALIDADE")]
+        public DateTime DataValidade { get; set; }
+
+        [Column("COD_FORNECEDOR")]
+        public int CodigoFornecedor { get; set; }
+
+        [Column("DESCRICAO_FORNECEDOR")]
+        public string DescricaoFornecedor { get; set; }
+
+        [Column("CNPJ_FORNECEDOR")]
+        public string CnpjFornecedor { get; set; }
+
+    }
+}
