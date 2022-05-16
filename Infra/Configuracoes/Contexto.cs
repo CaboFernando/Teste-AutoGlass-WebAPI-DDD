4461f265f13876fa6d062a900f53fcb872d73808
CaboFernando <fer.1972@hotmail.com>
2022-05-16 12:59:17 -0300

    - Add contexto na camada de Infra - Add RepositorioGenerico com os métodos genéricos de acesso ao banco

---
+++ Infra/Configuracoes/Contexto.cs
@@ -0,0 +1,42 @@
+﻿using Entidades.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Configuracoes
+{
+    public class Contexto : IdentityDbContext<ApplicationUser>
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+
+        DbSet<Produto> Produto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+        public string ObterStringConexao()
+        {
+            var str = "Server=DESKTOP-JDK2GPD; Database=dbTeste-AutoGlass; Trusted_Connection=True";
+            return str;
+        }
+    }
+}
