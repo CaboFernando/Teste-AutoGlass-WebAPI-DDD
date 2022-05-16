171dfd8172315a0aa5e6428227aa9b3dd1e88112
carlos.santos <carlos.santos@sp.agence.com.br>
2022-05-16 17:02:32 -0300

    -Criando migration para a criação do banco de dados

---
+++ Infra/Migrations/20220516200109_AlterandoProduto.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infra.Migrations
+{
+    public partial class AlterandoProduto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DESCRICAO_PRODUTO",
+                table: "TB_PRODUTO",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DESCRICAO_PRODUTO",
+                table: "TB_PRODUTO",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
