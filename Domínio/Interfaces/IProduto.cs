decd245d378717e7587d0b39339843d7e95c6931
CaboFernando <fer.1972@hotmail.com>
2022-05-16 12:12:58 -0300

    - Add na camada de Domínio a lógica e validação nos métodos de Adicionar, Atualizar e ListarProdutosAtivos

---
+++ Domínio/Interfaces/IProduto.cs
@@ -0,0 +1,16 @@
+﻿using Dominio.Interfaces.Genericos;
+using Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Interfaces
+{
+    public interface IProduto : IGenericos<Produto>
+    {
+        Task<List<Produto>> ListarProdutos(Expression<Func<Produto, bool>> exProduto);
+    }
+}
---
+++ Domínio/Servicos/ServicoProduto.cs
@@ -0,0 +1,47 @@
+﻿using Dominio.Interfaces;
+using Dominio.Interfaces.InterfacesServicos;
+using Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Servicos
+{
+    public class ServicoProduto : IServicoProduto
+    {
+        private readonly IProduto _produto;
+
+        public ServicoProduto(IProduto produto)
+        {
+            _produto = produto;
+        }
+
+        public async Task AdicionaProduto(Produto produto)
+        {
+            var validarDescricao = produto.ValidarPropriedadesString(produto.DescricaoProduto, "DescricaoProduto");
+            var validarDatas = produto.ValidarDataMaiorQueOutra(produto.DataFrabricacao, produto.DataValidade, "DataFrabricacao", "DataValidade");
+            if (validarDescricao && validarDatas)
+            {
+                await _produto.Adicionar(produto);
+            }
+        }
+
+        public async Task AtualizaProduto(Produto produto)
+        {
+            var validarCodigo = produto.ValidarPropriedadeDecimal(produto.CodigoProduto, "CodigoProduto");
+            var validarDescricao = produto.ValidarPropriedadesString(produto.DescricaoProduto, "DescricaoProduto");
+            var validarDatas = produto.ValidarDataMaiorQueOutra(produto.DataFrabricacao, produto.DataValidade, "DataFrabricacao", "DataValidade");
+            if (validarCodigo && validarDescricao && validarDatas)
+            {
+                await _produto.Atualizar(produto);
+            }
+        }
+
+        public async Task<List<Produto>> ListarProdutoAtivos()
+        {
+            return await _produto.ListarProdutos(x => x.SituacaoProduto);
+        }
+    }
+}
