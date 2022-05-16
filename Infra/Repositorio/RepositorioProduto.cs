a811c39d87327cf8022306b5bd498209b53bdd28
CaboFernando <fer.1972@hotmail.com>
2022-05-16 13:41:05 -0300

    - Alterando RepositorioProduto, para usar os métodos genéricos e executar as ações solicitadas no enunciado

--- Infra/Repositorio/RepositorioProduto.cs
+++ Infra/Repositorio/RepositorioProduto.cs
@@ -23,9 +23,68 @@ namespace Infra.Repositorio
 
         public async Task<List<Produto>> ListarProdutos(Expression<Func<Produto, bool>> exProduto)
         {
-            using (var db = new Contexto(_OptionsBuilder))
+            try
             {
-                return await db.Produto.Where(exProduto).AsNoTracking().ToListAsync();
+                using (var db = new Contexto(_OptionsBuilder))
+                {
+                    return await db.Produto.Where(exProduto).AsNoTracking().ToListAsync();
+                }
+            }
+            catch (Exception)
+            {
+                return new List<Produto>();
+            }
+        }
+
+        public async Task<Produto> BuscaPorCodigo(int id)
+        {
+            try
+            {
+                return await BuscarPorId(id);
+            }
+            catch (Exception)
+            {
+                return new Produto();
+            }
+        }
+
+        public async Task<bool> AdicionaProduto(Produto produto)
+        {
+            try
+            {
+                await Adicionar(produto);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> AtualizaProduto(Produto produto)
+        {
+            try
+            {
+                await AtualizaProduto(produto);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> RemoveProduto(Produto produto)
+        {
+            try
+            {
+                produto.SituacaoProduto = false;
+                await Atualizar(produto);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
             }
         }
     }
